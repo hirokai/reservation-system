533aea3c9aec67e9a07ac723354c7631f4b5d782
Hiro Kai <fullerenec84@gmail.com>
2023-12-17 21:45:28 +0900

    Client secret in env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,10 @@
 import { SvelteGoogleAuthHook } from 'svelte-google-auth/server';
 import type { Handle } from '@sveltejs/kit';
-
+import { CLIENT_SECRET } from '$env/static/private';
 // Import client credentials from json file
-import client_secret from '../client_secret.json';
+// import client_secret from '../client_secret.json';
 
-const auth = new SvelteGoogleAuthHook(client_secret.web);
+const auth = new SvelteGoogleAuthHook(JSON.parse(CLIENT_SECRET).web);
 
 export const handle: Handle = async ({ event, resolve }) => {
 	return await auth.handleAuth({ event, resolve });
