58a0d1f1960f969f23a766ec86793bfcb59ead0f
Hiro Kai <fullerenec84@gmail.com>
2023-12-17 21:43:35 +0900

    Env var

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,13 +1,15 @@
+import { DB_DATABASE, DB_PORT, DB_HOST, DB_USER, DB_PASSWORD } from '$env/static/private';
+
 import pg from 'pg-promise';
 const pgp = pg();
-
+console.log(process.env, DB_DATABASE);
 // DB接続情報
 const initOptions = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: "reservation_system",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+	host: DB_HOST,
+	port: Number(DB_PORT),
+	database: DB_DATABASE,
+	user: DB_USER,
+	password: DB_PASSWORD
 };
 
 console.log(initOptions);
